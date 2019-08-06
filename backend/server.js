cc133d9b7e8db97c6084ff9a6728b58dca407d5b
rbangamm <ravindu.angammana@gmail.com>
2019-08-06 15:07:43 -0400

    Change MongoDB link to test

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 // this is our MongoDB database
 const dbRoute =
-  'mongodb://<your-db-username-here>:<your-db-password-here>@ds249583.mlab.com:49583/fullstack_app';
+  'mongodb://127.0.0.1:27017/test';
 
 // connects our back end code with the database
 mongoose.connect(dbRoute, { useNewUrlParser: true });
